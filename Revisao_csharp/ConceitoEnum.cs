26be592091eadb4faf70c732758b7144b84d6438
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-16 20:57:19 -0300

    aplicação media de notas com C# console

---
+++ Revisao_csharp/ConceitoEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Revisao_csharp
+{
+    public enum ConceitoEnum
+    {
+        A,
+        B,
+        C,
+        D,
+        E
+    }
+}
---
+++ Revisao_csharp/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Revisao_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                        
+                        Console.WriteLine("Informe o nota do aluno: ");
+                        //Se ele conseguir fazer o parse ele ja insere na nota e adicona
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser decimal");
+                        }
+                        //aluno.Nota = Convert.ToDecimal(Console.ReadLine());
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        foreach (var al in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(al.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {al.Nome} - NOTA: {al.Nota}");
+                            }
+                        }
+                        break;
+                    case "3":
+                        
+                        //TODO: calcular média geral
+                        decimal notalTotal = 0;
+                        var numeroAlunos = 0;
+                        
+                        for (int i = 0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notalTotal = notalTotal + alunos[i].Nota;
+                                numeroAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notalTotal / numeroAlunos;
+                        ConceitoEnum conceitoGeral;
+
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = ConceitoEnum.E;
+                        }
+                        else if (mediaGeral < 4)
+                        {
+                            conceitoGeral = ConceitoEnum.D;
+                        }
+                        else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = ConceitoEnum.C;
+                        }
+                        else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = ConceitoEnum.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = ConceitoEnum.A;
+                        }
+                        
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO GERAL: {conceitoGeral}");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
